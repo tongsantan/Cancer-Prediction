1ac4b9eed0b7692838b0b91e9990145d70726dbe
tongsantan <vincente53@outlook.com>
2024-06-18 08:17:22 +0800

    updated

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,10 +1,14 @@
 import os
 import sys
 from dataclasses import dataclass
-from xgboost import XGBClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn import svm
 from sklearn.svm import SVC
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
+from sklearn.ensemble import HistGradientBoostingClassifier, GradientBoostingClassifier
 from sklearn.metrics import confusion_matrix
 from src.exception import CustomException
 from src import logger
@@ -31,30 +35,54 @@ class ModelTrainer:
                 test_array[:,-1]
             )
             models = {
-                        "Logistic Regression": LogisticRegression(random_state=42),
+                        "k-NN": KNeighborsClassifier(),
+                        "Decision Tree": DecisionTreeClassifier(random_state=42),
+                        "SVM": svm.SVC(),
+                        "BernoulliNB": BernoulliNB(),
+                        "LogisticRegression": LogisticRegression(random_state=42),
+                        "HistGradientBoostingClassifier": HistGradientBoostingClassifier(),
                         "XGBClassifier": XGBClassifier(),
-                        "SVM": svm.SVC()
+                        "GradientBoostClassifier": GradientBoostingClassifier()
 
                     }
             
             params={
-                        "Logistic Regression":{
-                            'C': [0.1], 
-                            'max_iter': [100], 
-                            'solver': ['liblinear']
+                        "k-NN": {"n_neighbors": [10, 20, 30 , 40]},
+                        
+                        "Decision Tree":{
+                            'max_depth': [1, 3, 5, 7],
+                            'max_features': [1, 10, 20 , 30]
                                         },
-
+                        
+                        "SVM":{  
+                            'kernel': ('linear', 'poly', 'rbf'),
+                            'C': [0.01, 0.1, 1, 10]},
+                
+                        "BernoulliNB":{},
+                
+                        "LogisticRegression":{
+                            'solver': ['lbfgs', 'liblinear', 'newton-cholesky'],
+                            'C': [0.01, 0.1, 1, 10],
+                            'max_iter': [100, 1000, 10000]
+                                            },
+                        
+                        "HistGradientBoostingClassifier":{
+                        'learning_rate':[.1,.01,.05,.001],
+                        'max_depth': [6,8,10]
+                            },
+                
                         "XGBClassifier":{
-                            'learning_rate': [0.1], 
-                            'n_estimators': [128]
+                        'learning_rate':[.1,.01,.001],
+                        'n_estimators': [8,16,32,64,128]
                                         },
-
-                        "SVM":{
-                            'C': [10], 
-                            'kernel': ['rbf']
+                
+                        "GradientBoostClassifier":{
+                        'learning_rate':[.1,.01,.05,.001],
+                        'subsample':[0.6,0.7,0.8,0.9],
+                        'n_estimators': [8,16,32,64,128]
+                                            }
+                                                        
                                 }
-                                            
-                    }
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
                                              models=models,param=params)
